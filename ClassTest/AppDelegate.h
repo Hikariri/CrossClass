6c627690ab77bec1fb4a619735ed3153ce1d0fc6
hikariri <nightheol@gmail.com>
2017-01-10 16:34:00 +0800

    Initial Commit

---
+++ ClassTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ClassTest
+//
+//  Created by Lin Lin on 2017/1/10.
+//  Copyright © 2017年 Lin Lin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ClassTest/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ClassTest
+//
+//  Created by Lin Lin on 2017/1/10.
+//  Copyright © 2017年 Lin Lin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
